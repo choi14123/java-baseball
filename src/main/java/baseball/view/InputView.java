291a0db4d9897b9fb09713ba9d38d2b936fdd922
choi14123 <ckdghks5565@gmail.com>
2024-01-02 22:22:17 +0900

    refactor: InputView

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,16 +1,28 @@
 package baseball.view;
 
+import java.util.Scanner;
 
 public class InputView {
-    private String PRINT_INPUT_NUMBERS_MESSAGE = "숫자를 입력해주세요 : ";
-    private String PRINT_INPUT_GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private final String PRINT_INPUT_NUMBERS_MESSAGE = "숫자를 입력해주세요 : ";
+    private final String PRINT_INPUT_GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
+    Scanner scanner = new Scanner(System.in);
 
-    public void printInputNumbersMessage() {
+    public String[] printInputNumbersMessage() {
         System.out.print(PRINT_INPUT_NUMBERS_MESSAGE);
+        String[] strArrUserNumbers = scanner.next().split("");
+        return strArrUserNumbers;
     }
 
-    public void printInputGameRestartMessage(int strike) {
+    public boolean setGame() {
+        int number = scanner.nextInt();
+        if (number != 1){
+            return false;
+        }
+        return true;
+    }
+
+    public void printInputGameRestartMessage() {
         System.out.print(PRINT_INPUT_GAME_RESTART_MESSAGE);
     }
 }
