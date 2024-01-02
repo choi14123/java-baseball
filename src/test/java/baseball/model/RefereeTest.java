f9874875e82d878b0c3faa208d58197270044746
choi14123 <ckdghks5565@gmail.com>
2024-01-02 22:23:30 +0900

    test: RefereeTest

--- src/test/java/baseball/model/RefereeTest.java
+++ src/test/java/baseball/model/RefereeTest.java
@@ -4,8 +4,7 @@ package baseball.model;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 class RefereeTest {
@@ -13,22 +12,16 @@ class RefereeTest {
     @DisplayName("컴퓨터와 사용자의 숫자를 비교하여 볼 갯수와 스트라이크 갯수를 반환한다.")
     void twoBallOneStrike() {
         //given
-        List<Integer> computerNumbers = new ArrayList<>();
-        List<Integer> userNumbers = new ArrayList<>();
-
-        computerNumbers.add(1);
-        computerNumbers.add(2);
-        computerNumbers.add(3);
-
-        userNumbers.add(1);
-        userNumbers.add(3);
-        userNumbers.add(2);
+        List<Integer> computerNumbers = Arrays.asList(1, 2, 3);
+        List<Integer> userNumbers = Arrays.asList(1, 3, 2);
 
         BaseballNumbers computerBaseballNumbers = new BaseballNumbers(computerNumbers);
         BaseballNumbers userBaseballNumbers = new BaseballNumbers(userNumbers);
 
+        Referee referee = new Referee();
+
         //when
-        Referee referee = new Referee(computerBaseballNumbers, userBaseballNumbers);
+       referee.compare(computerBaseballNumbers, userBaseballNumbers);
 
         //then
         Assertions.assertThat(referee.getBall()).isEqualTo(2);
