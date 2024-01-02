ace53b65972622a3872818ded65bc24563c95435
choi14123 <ckdghks5565@gmail.com>
2024-01-02 22:21:00 +0900

    style: BaseballNumber

--- src/main/java/baseball/model/BaseballNumber.java
+++ src/main/java/baseball/model/BaseballNumber.java
@@ -1,6 +1,8 @@
 package baseball.model;
 
 public class BaseballNumber {
+    private final int MIN_BASEBALL_NUMBER_SIZE = 1;
+    private final int MAX_BASEBALL_NUMBER_SIZE = 9;
     private int number;
 
     public BaseballNumber(int number) {
@@ -13,8 +15,8 @@ public class BaseballNumber {
     }
 
     private void validateNumber(int number) {
-        if (number < 1 || 9 < number) {
-            throw new IllegalArgumentException();
+        if (number < MIN_BASEBALL_NUMBER_SIZE || MAX_BASEBALL_NUMBER_SIZE < number) {
+            throw new IllegalArgumentException("1부터 9까지의 숫자만 입력해야 합니다.");
         }
     }
 }
