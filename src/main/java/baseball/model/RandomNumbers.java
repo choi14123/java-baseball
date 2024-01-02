bb2a9f5f9a5b5cebb1c847048cde9a097859aee2
choi14123 <ckdghks5565@gmail.com>
2024-01-02 22:23:01 +0900

    refactor: RandomNumbers

--- src/main/java/baseball/model/RandomNumbers.java
+++ src/main/java/baseball/model/RandomNumbers.java
@@ -5,22 +5,22 @@ import java.util.List;
 import java.util.Random;
 
 public class RandomNumbers {
-    private final static int COMPUTER_NUMBER_SIZE = 3;
-    private List<Integer> computerNumbers = new ArrayList<>();
+    private final int COMPUTER_NUMBER_SIZE = 3;
+    private final int MAX_RANDOM_NUMBER_SIZE = 9;
+    private List<Integer> computerNumber = new ArrayList<>();
     private Random random = new Random();
 
     public List<Integer> create() {
-        while (computerNumbers.size() < COMPUTER_NUMBER_SIZE) {
-            int number;
-            number = random.nextInt(9);
-            if (!computerNumbers.contains(number)) {
-                computerNumbers.add(number);
+        while (computerNumber.size() < COMPUTER_NUMBER_SIZE) {
+            int number = random.nextInt(MAX_RANDOM_NUMBER_SIZE) + 1;
+            if (!computerNumber.contains(number)) {
+                computerNumber.add(number);
             }
         }
-        return computerNumbers;
+        return computerNumber;
     }
 
-    public void deleteComputerNumbers() {
-        computerNumbers.clear();
+    public BaseballNumbers set(List<Integer> numbers) {
+        return new BaseballNumbers(numbers);
     }
 }
