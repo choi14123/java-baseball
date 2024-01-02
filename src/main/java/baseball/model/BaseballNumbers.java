595cd20a83ec969aacc846c20b91337907335701
choi14123 <ckdghks5565@gmail.com>
2024-01-02 22:21:16 +0900

    style: BaseballNumbers

--- src/main/java/baseball/model/BaseballNumbers.java
+++ src/main/java/baseball/model/BaseballNumbers.java
@@ -5,12 +5,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class BaseballNumbers {
-    // TODO: 12/19/23
-    // convertToBaseballNumber for문 스트림으로 해결
-    // point 인티저 리스트를 베이스볼 리스트로 변경
+    private final int BASEBALL_NUMBER_SIZE = 3;
     private List<BaseballNumber> numbers = new ArrayList<>();
-
-    private final static int BASEBALL_NUMBER_SIZE = 3;
 
     public BaseballNumbers(List<Integer> numbers) {
         validateSize(numbers);
@@ -31,15 +27,17 @@ public class BaseballNumbers {
     }
 
     private void validateDuplication(List<Integer> numbers) {
-        List<Integer> list = numbers.stream().distinct().collect(Collectors.toList());
+        List<Integer> list = numbers.stream()
+                .distinct()
+                .collect(Collectors.toList());
         if (numbers.size() != list.size()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("중복된 숫자가 입력되었습니다.");
         }
     }
 
     private void validateSize(List<Integer> numbers) {
         if (numbers.size() != BASEBALL_NUMBER_SIZE) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("숫자 3자리를 입력해야 합니다.");
         }
     }
 
