d0a5a05a737f76d991ca86d2162f97d924ca44b3
choi14123 <ckdghks5565@gmail.com>
2024-01-02 22:23:10 +0900

    test: RandomNumbersTest

--- src/test/java/baseball/model/RandomNumbersTest.java
+++ src/test/java/baseball/model/RandomNumbersTest.java
@@ -4,23 +4,18 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 
 class RandomNumbersTest {
-
     @Test
     @DisplayName("랜덤한 숫자 3개 생성한다.")
     void createRandomNumbersSize() {
-
         //given
         RandomNumbers randomNumbers = new RandomNumbers();
-        BaseballNumbers computerNumbers = new BaseballNumbers(randomNumbers.create());
 
         //when
-        List<BaseballNumber> numbers = computerNumbers.getNumbers();
+        int randomNumberSize = randomNumbers.set(randomNumbers.create()).getNumbers().size();
 
         //then
-        Assertions.assertThat(numbers.size()).isEqualTo(3);
+        Assertions.assertThat(randomNumberSize).isEqualTo(3);
     }
 }
