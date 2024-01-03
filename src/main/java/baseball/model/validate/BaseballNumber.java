9216666af72274572a6086731b5707f280e14f09
choi14123 <ckdghks5565@gmail.com>
2024-01-03 15:06:13 +0900

    refactor: BaseballNumber

---
+++ src/main/java/baseball/model/validate/BaseballNumber.java
@@ -0,0 +1,23 @@
+package baseball.model.validate;
+
+public class BaseballNumber {
+    private static final int MIN_BASEBALL_NUMBER_SIZE = 1;
+    private static final int MAX_BASEBALL_NUMBER_SIZE = 9;
+    private static final String VALIDATE_NUMBER_SIZE_EXCEPTION_MESSAGE = "1부터 9까지의 숫자만 입력해야 합니다.";
+    private final int number;
+
+    public BaseballNumber(int number) {
+        validateNumber(number);
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    private void validateNumber(int number) {
+        if (number < MIN_BASEBALL_NUMBER_SIZE || MAX_BASEBALL_NUMBER_SIZE < number) {
+            throw new IllegalArgumentException(VALIDATE_NUMBER_SIZE_EXCEPTION_MESSAGE);
+        }
+    }
+}
