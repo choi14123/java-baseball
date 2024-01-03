5433008dba0d905cfc05c8957631121978c386ca
choi14123 <ckdghks5565@gmail.com>
2024-01-03 21:46:06 +0900

    style: OutputView

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -2,7 +2,7 @@ package baseball.view;
 
 
 public class OutputView {
-    private final String PRINT_START_GAME_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String PRINT_START_GAME_MESSAGE = "숫자 야구 게임을 시작합니다.";
 
     public void printStartGameMessage() {
         System.out.println(PRINT_START_GAME_MESSAGE);
