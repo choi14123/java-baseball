ffe0c779bcb618dd31fd0aceb36522953ea16d1f
choi14123 <ckdghks5565@gmail.com>
2024-01-03 15:06:21 +0900

    refactor: BaseballNumbers

--- src/main/java/baseball/model/validate/BaseballNumbers.java
+++ src/main/java/baseball/model/validate/BaseballNumbers.java
@@ -1,15 +1,18 @@
-package baseball.model;
+package baseball.model.validate;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+
 public class BaseballNumbers {
     private final int BASEBALL_NUMBER_SIZE = 3;
+    private static final String VALIDATE_DUPLICATION_EXCEPTION_MESSAGE = "중복된 숫자가 입력되었습니다.";
+    private static final String VALIDATE_NUMBER_LENGTH_EXCEPTION_MESSAGE = "중복된 숫자가 입력되었습니다.";
     private List<BaseballNumber> numbers = new ArrayList<>();
 
     public BaseballNumbers(List<Integer> numbers) {
-        validateSize(numbers);
+        validateLength(numbers);
         validateDuplication(numbers);
         List<BaseballNumber> inputtedNumbers = convertToBaseballNumber(numbers);
         this.numbers = inputtedNumbers;
@@ -17,7 +20,6 @@ public class BaseballNumbers {
 
     public List<BaseballNumber> convertToBaseballNumber(List<Integer> numbers) {
         List<BaseballNumber> inputtedNumbers = new ArrayList<>();
-
         for (int i = 0; i < BASEBALL_NUMBER_SIZE; i++) {
             int num = numbers.get(i);
             BaseballNumber baseballNumber = new BaseballNumber(num);
@@ -31,13 +33,13 @@ public class BaseballNumbers {
                 .distinct()
                 .collect(Collectors.toList());
         if (numbers.size() != list.size()) {
-            throw new IllegalArgumentException("중복된 숫자가 입력되었습니다.");
+            throw new IllegalArgumentException(VALIDATE_DUPLICATION_EXCEPTION_MESSAGE);
         }
     }
 
-    private void validateSize(List<Integer> numbers) {
+    private void validateLength(List<Integer> numbers) {
         if (numbers.size() != BASEBALL_NUMBER_SIZE) {
-            throw new IllegalArgumentException("숫자 3자리를 입력해야 합니다.");
+            throw new IllegalArgumentException(VALIDATE_NUMBER_LENGTH_EXCEPTION_MESSAGE);
         }
     }
 
