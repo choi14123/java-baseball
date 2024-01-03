b4cb3949e06e60e15959c9331259fe22733b5384
choi14123 <ckdghks5565@gmail.com>
2024-01-03 20:30:54 +0900

    style: BaseballNumber

--- src/main/java/baseball/model/baseballnumber/BaseballNumber.java
+++ src/main/java/baseball/model/baseballnumber/BaseballNumber.java
@@ -4,6 +4,7 @@ public class BaseballNumber {
     private static final int MIN_BASEBALL_NUMBER_SIZE = 1;
     private static final int MAX_BASEBALL_NUMBER_SIZE = 9;
     private static final String VALIDATE_NUMBER_SIZE_EXCEPTION_MESSAGE = "1부터 9까지의 숫자만 입력해야 합니다.";
+
     private final int number;
 
     public BaseballNumber(int number) {
