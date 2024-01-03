4f839feb537187659b2dfc7d3137c2364d354308
choi14123 <ckdghks5565@gmail.com>
2024-01-03 15:19:44 +0900

    test: UserNumbersTest

--- src/test/java/baseball/model/UserNumberTest.java
+++ src/test/java/baseball/model/UserNumberTest.java
@@ -4,10 +4,12 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class UserNumberTest {
     @Test
     @DisplayName("사용자가 입력한 숫자가 3자리인지 확인한다.")
-    void validateSize() {
+    void validateLength() {
         //given
         UserNumbers userNumbers = new UserNumbers();
 
@@ -17,5 +19,19 @@ public class UserNumberTest {
         //when
         //then
         Assertions.assertThat(userNumbers.convertToBaseballNumber(strArrUserNumber).getNumbers().size()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("사용자가 입력한 숫자가 3자리가 아닐시 IllegalArgumentException 예외 발생.")
+    void validateLengthThrowNewException() {
+        //given
+        UserNumbers userNumbers = new UserNumbers();
+
+        String strUserNumbers = "12";
+        String[] strArrUserNumber = strUserNumbers.split("");
+
+        //when
+        //then
+        assertThatThrownBy(() -> userNumbers.convertToBaseballNumber(strArrUserNumber)).isInstanceOf(IllegalArgumentException.class);
     }
 }
