e3e8248ba88f83b50ee80a4272018a74364f542b
choi14123 <ckdghks5565@gmail.com>
2024-01-03 21:45:58 +0900

    style: InputView

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -3,8 +3,8 @@ package baseball.view;
 import java.util.Scanner;
 
 public class InputView {
-    private final String PRINT_INPUT_NUMBERS_MESSAGE = "숫자를 입력해주세요 : ";
-    private final String PRINT_INPUT_GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String PRINT_INPUT_NUMBERS_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String PRINT_INPUT_GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     Scanner scanner = new Scanner(System.in);
 
