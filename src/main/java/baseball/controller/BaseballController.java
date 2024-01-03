91198b057a815e149d395a972135bbf9782916a7
choi14123 <ckdghks5565@gmail.com>
2024-01-03 21:44:44 +0900

    refactor: BaseballController

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -1,30 +1,35 @@
 package baseball.controller;
 
-import baseball.model.baseballnumber.RandomNumbers;
 import baseball.model.Referee;
+import baseball.model.baseballnumber.BaseballNumbers;
+import baseball.model.baseballnumber.RandomNumbersFactory;
 import baseball.model.baseballnumber.UserNumbers;
 import baseball.view.InputView;
 import baseball.view.OutputView;
 
+import java.util.List;
+
 public class BaseballController {
-    private final int THREE_STRIKE_CONDITION = 3;
-    InputView inputView = new InputView();
-    OutputView outputView = new OutputView();
-    RandomNumbers randomNumbers = new RandomNumbers();
-    UserNumbers userNumbers = new UserNumbers();
-    Referee referee = new Referee();
+    private static final int THREE_STRIKE_CONDITION = 3;
+    private InputView inputView = new InputView();
+    private OutputView outputView = new OutputView();
+    private RandomNumbersFactory randomNumbers = new RandomNumbersFactory();
+    private UserNumbers userNumbers = new UserNumbers();
+    private Referee referee = new Referee();
 
     public void start() {
         boolean setGame = true;
-
         outputView.printStartGameMessage();
         while (setGame) {
-            referee.compare(randomNumbers.set(randomNumbers.create()), userNumbers.convertToBaseballNumber(inputView.printInputNumbersMessage()));
-            outputView.printRefereeMessage(referee.getBall(), referee.getStrike());
-            if (referee.getStrike() == THREE_STRIKE_CONDITION) {
+            BaseballNumbers computerNumbersList = new BaseballNumbers(randomNumbers.create());
+            BaseballNumbers userNumbersList = new BaseballNumbers(userNumbers.convertToBaseballNumber(inputView.printInputNumbersMessage()));
+            int countBall = referee.countBall(computerNumbersList, userNumbersList);
+            int countStrike = referee.countStrike(computerNumbersList, userNumbersList);
+            outputView.printRefereeMessage(countBall, countStrike);
+            if (countStrike == THREE_STRIKE_CONDITION) {
                 inputView.printInputGameRestartMessage();
                 setGame = inputView.setGame();
-                randomNumbers = new RandomNumbers();
+                randomNumbers = new RandomNumbersFactory();
                 randomNumbers.create();
             }
             userNumbers = new UserNumbers();
