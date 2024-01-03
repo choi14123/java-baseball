297d223d176e31d918db1db5961f7388bfb03868
choi14123 <ckdghks5565@gmail.com>
2024-01-03 21:48:03 +0900

    style: UserNumbers

--- src/main/java/baseball/model/baseballnumber/UserNumbers.java
+++ src/main/java/baseball/model/baseballnumber/UserNumbers.java
@@ -4,20 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserNumbers {
-    private final int USER_INPUT_NUMBERS_LENGTH = 3;
+    private final int userInputNumbersLength = 3;
     private List<Integer> userNumbersList = new ArrayList<>();
 
-    public BaseballNumbers convertToBaseballNumber(String[] strArr) {
+    public List<Integer> convertToBaseballNumber(String[] strArr) {
         validateSize(strArr);
-        for (int i = 0; i < USER_INPUT_NUMBERS_LENGTH; i++) {
+        for (int i = 0; i < userInputNumbersLength; i++) {
             userNumbersList.add(Integer.valueOf(strArr[i]));
         }
-        BaseballNumbers userNumbers = new BaseballNumbers(userNumbersList);
-        return userNumbers;
+        return userNumbersList;
     }
 
     private void validateSize(String[] strArr) {
-        if (strArr.length != USER_INPUT_NUMBERS_LENGTH) {
+        if (strArr.length != userInputNumbersLength) {
             throw new IllegalArgumentException("숫자 3자리를 입력해야 합니다.");
         }
     }
