4879ff2718ab08bb647e70a99dbc43e9373e5e37
choi14123 <ckdghks5565@gmail.com>
2024-01-06 21:40:11 +0900

    style: RandomNumbersFactory

--- src/main/java/baseball/model/baseballnumber/RandomNumbersFactory.java
+++ src/main/java/baseball/model/baseballnumber/RandomNumbersFactory.java
@@ -1,23 +1,25 @@
 package baseball.model.baseballnumber;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 public class RandomNumbersFactory {
     private static final int COMPUTER_NUMBER_SIZE = 3;
     private static final int MAX_RANDOM_NUMBER_SIZE = 9;
 
-    private List<Integer> computerNumber = new ArrayList<>();
     private Random random = new Random();
+    private List<Integer> computerNumberList = new ArrayList<>();
 
     public List<Integer> create() {
-        while (computerNumber.size() < COMPUTER_NUMBER_SIZE) {
+        while (computerNumberList.size() < COMPUTER_NUMBER_SIZE) {
             int number = random.nextInt(MAX_RANDOM_NUMBER_SIZE) + 1;
-            if (!computerNumber.contains(number)) {
-                computerNumber.add(number);
+            if (!computerNumberList.contains(number)) {
+                computerNumberList.add(number);
             }
         }
-        return computerNumber;
+        return computerNumberList;
     }
 }
