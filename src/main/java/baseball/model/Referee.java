c9da0b3f35da803e63dd093cb55f66919e143639
choi14123 <ckdghks5565@gmail.com>
2024-01-06 21:40:40 +0900

    refactor: Referee countStrike 수정

--- src/main/java/baseball/model/Referee.java
+++ src/main/java/baseball/model/Referee.java
@@ -2,13 +2,25 @@ package baseball.model;
 
 import baseball.model.baseballnumber.BaseballNumbers;
 
+import java.util.stream.IntStream;
+
+
 public class Referee {
-    private final int threeDigitNumber = 3;
+
+    private static final int START = 0;
+    private static final int NUMBER_LENGTH = 3;
+    private static final int THREE_DIGIT_NUMBER = 3;
+
+    public int countStrike(BaseballNumbers computer, BaseballNumbers user) {
+        return (int) IntStream.range(START, NUMBER_LENGTH)
+                .filter(num -> computer.getNumbers().get(num).getNumber() == user.getNumbers().get(num).getNumber())
+                .count();
+    }
 
     public int countBall(BaseballNumbers computer, BaseballNumbers user) {
         int ballCountNumber = 0;
-        for (int i = 0; i < threeDigitNumber; i++) {
-            for (int j = 0; j < threeDigitNumber; j++) {
+        for (int i = 0; i < THREE_DIGIT_NUMBER; i++) {
+            for (int j = 0; j < THREE_DIGIT_NUMBER; j++) {
                 if (computer.getNumbers().get(i).getNumber() == user.getNumbers().get(j).getNumber()) {
                     if (i != j) {
                         ballCountNumber++;
@@ -17,15 +29,5 @@ public class Referee {
             }
         }
         return ballCountNumber;
-    }
-
-    public int countStrike(BaseballNumbers computer, BaseballNumbers user) {
-        int strikeCountNumber = 0;
-        for (int i = 0; i < threeDigitNumber; i++) {
-            if (computer.getNumbers().get(i).getNumber() == user.getNumbers().get(i).getNumber()) {
-                strikeCountNumber++;
-            }
-        }
-        return strikeCountNumber;
     }
 }
