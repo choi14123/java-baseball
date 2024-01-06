716d5f8d331e4fa99e556c8185110175d9d6f54d
choi14123 <ckdghks5565@gmail.com>
2024-01-06 16:37:28 +0900

    style: Application

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,9 +2,10 @@ package baseball;
 
 import baseball.controller.BaseballController;
 
+
 public class Application {
     public static void main(String[] args) {
         BaseballController baseballController = new BaseballController();
         baseballController.start();
-    }
+     }
 }
