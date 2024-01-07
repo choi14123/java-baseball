4eaf0c806c641af51c7a3e97187f5204550c8216
choi14123 <ckdghks5565@gmail.com>
2024-01-07 11:49:11 +0900

    test: UserNumberTest

--- src/test/java/baseball/model/baseballnumber/UserNumberTest.java
+++ src/test/java/baseball/model/baseballnumber/UserNumberTest.java
@@ -1,6 +1,5 @@
 package baseball.model.baseballnumber;
 
-import baseball.model.baseballnumber.UserNumbers;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
