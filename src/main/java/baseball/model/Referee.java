5817e59370ce09409343a5d74644a01ea97254db
choi14123 <ckdghks5565@gmail.com>
2024-01-07 20:30:17 +0900

    style: countStrike, countBall 메서드 명, ballCount, strikeCount로 변경

--- src/main/java/baseball/model/Referee.java
+++ src/main/java/baseball/model/Referee.java
@@ -12,19 +12,19 @@ public class Referee {
     private static final int START = 0;
     private static final int NUMBER_LENGTH = 3;
 
-    public int countStrike(BaseballNumbers computers, BaseballNumbers users) {
+    public int strikeCount(BaseballNumbers computers, BaseballNumbers users) {
         return (int) IntStream.range(START, NUMBER_LENGTH)
                 .filter(index -> computers.getBaseballNumberValue(index) == users.getBaseballNumberValue(index))
                 .count();
     }
 
-    public int countBall(BaseballNumbers computers, BaseballNumbers users) {
+    public int ballCount(BaseballNumbers computers, BaseballNumbers users) {
         List<Integer> computerList = Arrays.asList(computers.getBaseballNumberValue(0), computers.getBaseballNumberValue(1), computers.getBaseballNumberValue(2));
         List<Integer> uesrList = Arrays.asList(users.getBaseballNumberValue(0), users.getBaseballNumberValue(1), users.getBaseballNumberValue(2));
 
         return (int) (computerList.stream()
                 .filter(randomNumber -> uesrList.stream().anyMatch(Predicate.isEqual(randomNumber)))
                 .count()
-                - countStrike(users, computers));
+                - strikeCount(users, computers));
     }
 }
