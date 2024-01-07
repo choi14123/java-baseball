73fbccd9b8b0c16430ed9fe3cc8e7de6f86455df
choi14123 <ckdghks5565@gmail.com>
2024-01-07 20:29:47 +0900

    refactor: printInputGameRestartMessage 메서드 OutputView로 이

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -3,9 +3,14 @@ package baseball.view;
 
 public class OutputView {
     private static final String PRINT_START_GAME_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String PRINT_INPUT_GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public void printStartGameMessage() {
         System.out.println(PRINT_START_GAME_MESSAGE);
+    }
+
+    public void printInputGameRestartMessage() {
+        System.out.print(PRINT_INPUT_GAME_RESTART_MESSAGE);
     }
 
     public void printNothing() {
