e484b3a94b813c51d9b2d6bbc0681790ac9080ab
choi14123 <ckdghks5565@gmail.com>
2024-01-07 18:38:02 +0900

    refactor: BaseballController

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -3,7 +3,6 @@ package baseball.controller;
 import baseball.model.Referee;
 import baseball.model.baseballnumber.BaseballNumbers;
 import baseball.model.baseballnumber.RandomNumbersFactory;
-import baseball.model.baseballnumber.UserNumbers;
 import baseball.view.InputView;
 import baseball.view.OutputView;
 
@@ -13,26 +12,55 @@ public class BaseballController {
     private InputView inputView = new InputView();
     private OutputView outputView = new OutputView();
     private RandomNumbersFactory randomNumbers = new RandomNumbersFactory();
-    private UserNumbers userNumbers = new UserNumbers();
     private Referee referee = new Referee();
 
     public void start() {
-        boolean setGame = true;
+        boolean isPlaying = true;
         outputView.printStartGameMessage();
-        while (setGame) {
+        while (isPlaying) {
             BaseballNumbers computerNumbersList = new BaseballNumbers(randomNumbers.create());
-            BaseballNumbers userNumbersList = new BaseballNumbers(userNumbers.convertToBaseballNumber(inputView.printInputNumbersMessage()));
+            BaseballNumbers userNumbersList = new BaseballNumbers(inputView.printInputNumbersMessage());
             int countBall = referee.countBall(computerNumbersList, userNumbersList);
             int countStrike = referee.countStrike(computerNumbersList, userNumbersList);
-            outputView.printRefereeMessage(countBall, countStrike);
+            printResult(countBall, countStrike);
             if (countStrike == THREE_STRIKE_CONDITION) {
                 inputView.printInputGameRestartMessage();
-                setGame = inputView.setGame();
+                isPlaying = inputView.isPlaying();
                 randomNumbers = new RandomNumbersFactory();
                 randomNumbers.create();
             }
-            userNumbers = new UserNumbers();
+            inputView = new InputView();
             referee = new Referee();
+        }
+    }
+
+    private void printResult(int countBall, int countStrike) {
+        isNothing(countBall, countStrike);
+        hasBallOrStrike(countBall, countStrike);
+        isThreeStrike(countStrike);
+    }
+
+    private void isNothing(int ballCount, int strikeCount) {
+        if (ballCount == 0 && strikeCount == 0) {
+            outputView.printNothing();
+        }
+    }
+
+    private void hasBallOrStrike(int ballCount, int strikeCount) {
+        if (ballCount > 0 && strikeCount == 0) {
+            outputView.printBall(ballCount);
+        }
+        if (strikeCount > 0 && ballCount == 0) {
+            outputView.printStrike(strikeCount);
+        }
+        if (ballCount > 0 && strikeCount > 0) {
+            outputView.printBallAndStrike(ballCount, strikeCount);
+        }
+    }
+
+    private void isThreeStrike(int strikeCount) {
+        if (strikeCount == 3) {
+            outputView.printThreeStrike();
         }
     }
 }
