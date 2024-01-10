db215bd3dcbdd26fc98ab27c7e9fe3fa45ed7b66
choi14123 <ckdghks5565@gmail.com>
2024-01-10 20:01:09 +0900

    test: "RefereeTest"

--- src/test/java/baseball/model/RefereeTest.java
+++ src/test/java/baseball/model/RefereeTest.java
@@ -9,35 +9,35 @@ import org.junit.jupiter.api.Test;
 
 class RefereeTest {
     @Test
-    @DisplayName("컴퓨터와 사용자의 숫자를 비교하여 볼 갯수 반환한다.")
+    @DisplayName("컴퓨터의 숫자와 사용자가 입력한 숫자를 비교하여 볼 갯수를 반환한다.")
     void ballCountTest() {
         //given
         List<Integer> computerNumbers = Arrays.asList(1, 2, 3);
         List<Integer> userNumbers = Arrays.asList(1, 3, 2);
 
+        //when
         BaseballNumbers computerBaseballNumbers = new BaseballNumbers(computerNumbers);
         BaseballNumbers userBaseballNumbers = new BaseballNumbers(userNumbers);
 
         Referee referee = new Referee();
 
-        //when
         //then
         Assertions.assertThat(referee.ballCount(computerBaseballNumbers, userBaseballNumbers)).isEqualTo(2);
     }
 
     @Test
-    @DisplayName("컴퓨터와 사용자의 숫자를 비교하여 스트라이크 갯수 반환한다.")
+    @DisplayName("컴퓨터의 숫자와 사용자가 입력한 숫자를 비교하여 스트라이크 갯수를 반환한다.")
     void strikeCountTest() {
         //given
         List<Integer> computerNumbers = Arrays.asList(1, 2, 3);
         List<Integer> userNumbers = Arrays.asList(1, 3, 2);
 
+        //when
         BaseballNumbers computerBaseballNumbers = new BaseballNumbers(computerNumbers);
         BaseballNumbers userBaseballNumbers = new BaseballNumbers(userNumbers);
 
         Referee referee = new Referee();
 
-        //when
         //then
         Assertions.assertThat(referee.strikeCount(computerBaseballNumbers, userBaseballNumbers)).isEqualTo(1);
     }
