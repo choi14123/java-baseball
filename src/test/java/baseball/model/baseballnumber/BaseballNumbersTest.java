f9223961823ee08f17fd227541b6b10718981c24
Choi Chang Hwan <63833865+choi14123@users.noreply.github.com>
2024-01-10 22:27:42 +0900

    test: "BaseballNumbersTest"

--- src/test/java/baseball/model/baseballnumber/BaseballNumbersTest.java
+++ src/test/java/baseball/model/baseballnumber/BaseballNumbersTest.java
@@ -3,7 +3,6 @@ package baseball.model.baseballnumber;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
@@ -14,28 +13,27 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 class BaseballNumbersTest {
 
-    @Test
-    @DisplayName("convertToBaseballNumber를 실행하면 3개의 숫자가 저장된다.")
-    void convertToBaseballNumberTest() {
-        //given
-        List<Integer> numbers = Arrays.asList(7, 8, 9);
-
-        //when
-        //then
-        assertThat(new BaseballNumbers(numbers).getNumbers().size()).isEqualTo(3);
-    }
-
     private static Stream<Arguments> notThreeSizeNumbersProvide() {
         return Stream.of(
-                Arguments.of(Arrays.asList(1)),
-                Arguments.of(Arrays.asList(1, 2)),
-                Arguments.of(Arrays.asList(1, 2, 3, 4))
+                Arguments.of(List.of()),
+                Arguments.of(List.of(1, 2)),
+                Arguments.of(List.of(1, 2, 3, 4))
         );
     }
 
     private static Stream<Arguments> duplicationNumbersProvide() {
-        return Stream.of(Arguments.of(Arrays.asList(1, 1, 1)), Arguments.of(Arrays.asList(1, 2, 1)),
-                Arguments.of(Arrays.asList(1, 2, 2)));
+        return Stream.of(Arguments.of(List.of(1, 1, 1)),
+                Arguments.of(List.of(1, 2, 1)),
+                Arguments.of(List.of(1, 2, 2)));
+    }
+
+    @Test
+    @DisplayName("convertToBaseballNumber를 실행하면 3개의 숫자가 저장되는지 확인한다.")
+    void convertToBaseballNumberTest() {
+        //given
+        List<Integer> numbers = List.of(7, 8, 9);
+        //when, then
+        assertThat(new BaseballNumbers(numbers).getNumbers().size()).isEqualTo(3);
     }
 
     @ParameterizedTest
@@ -51,7 +49,7 @@ class BaseballNumbersTest {
     @ParameterizedTest
     @MethodSource("duplicationNumbersProvide")
     @DisplayName("객체를 생성 할 때, 중복 값이 있으면 IllegalArgumentException 예외 발생")
-    void validateDuplicationValueTest(List<Integer> duplicationNumbers) {
+    void validateDuplicationTest(List<Integer> duplicationNumbers) {
         //when, then
         assertThatThrownBy(() -> new BaseballNumbers(duplicationNumbers))
                 .isInstanceOf(IllegalArgumentException.class)
