df9d0e70d03ba02a61ee280444fb5bacb281a7a5
Choi Chang Hwan <63833865+choi14123@users.noreply.github.com>
2024-01-10 22:28:03 +0900

    test: "RefereeTest"

--- src/test/java/baseball/model/RefereeTest.java
+++ src/test/java/baseball/model/RefereeTest.java
@@ -1,6 +1,7 @@
 package baseball.model;
 
 import baseball.model.baseballnumber.BaseballNumbers;
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,8 +11,8 @@ class RefereeTest {
     @DisplayName("컴퓨터의 숫자와 사용자가 입력한 숫자를 비교하여 볼 갯수를 반환한다.")
     void ballCountTest() {
         //given
-        List<Integer> computerNumbers = Arrays.asList(1, 2, 3);
-        List<Integer> userNumbers = Arrays.asList(1, 3, 2);
+        List<Integer> computerNumbers = List.of(1, 2, 3);
+        List<Integer> userNumbers = List.of(1, 3, 2);
 
         //when
         BaseballNumbers computerBaseballNumbers = new BaseballNumbers(computerNumbers);
@@ -27,8 +28,8 @@ class RefereeTest {
     @DisplayName("컴퓨터의 숫자와 사용자가 입력한 숫자를 비교하여 스트라이크 갯수를 반환한다.")
     void strikeCountTest() {
         //given
-        List<Integer> computerNumbers = Arrays.asList(1, 2, 3);
-        List<Integer> userNumbers = Arrays.asList(1, 3, 2);
+        List<Integer> computerNumbers = List.of(1, 2, 3);
+        List<Integer> userNumbers = List.of(1, 3, 2);
 
         //when
         BaseballNumbers computerBaseballNumbers = new BaseballNumbers(computerNumbers);
