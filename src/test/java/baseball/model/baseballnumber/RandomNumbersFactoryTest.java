2cb3d636b5671a8f19317dca9316268e3620338a
Choi Chang Hwan <63833865+choi14123@users.noreply.github.com>
2024-01-10 22:40:13 +0900

    test: "RandomNumbersFactory"

--- src/test/java/baseball/model/baseballnumber/RandomNumbersFactoryTest.java
+++ src/test/java/baseball/model/baseballnumber/RandomNumbersFactoryTest.java
@@ -8,12 +8,9 @@ class RandomNumbersFactoryTest {
     @Test
     @DisplayName("랜덤한 숫자를 생성했을 때, 숫자가 3개인지 확인한다.")
     void createRandomNumbersSizeTest() {
-        //given
-        RandomNumbersFactory randomNumbers = new RandomNumbersFactory();
-
         //when
+        RandomNumbersFactory randomNumbers = new RandomNumbersFactory();
         BaseballNumbers baseballNumbers = new BaseballNumbers(randomNumbers.create());
-
         //then
         Assertions.assertThat(baseballNumbers.getNumbers().size()).isEqualTo(3);
     }
