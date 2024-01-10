a5fe6172edfc4dac799ae3778bb4ce3a8955af57
choi14123 <ckdghks5565@gmail.com>
2024-01-10 20:00:51 +0900

    test: "BaseballNumberTest"

--- src/test/java/baseball/model/baseballnumber/BaseballNumberTest.java
+++ src/test/java/baseball/model/baseballnumber/BaseballNumberTest.java
@@ -11,9 +11,7 @@ class BaseballNumberTest {
     @ValueSource(ints = {0, 10})
     @DisplayName("숫자 야구 넘버는 1 ~ 9 까지의 숫자만 허용한다. 이외의 숫자가 들어오면 IllegalArgumentException 예외 발생")
     void validateNumberTest(int input) {
-        //given
-        //when
-        //then
+        //when, then
         assertThatThrownBy(() -> new BaseballNumber(input)).isInstanceOf(IllegalArgumentException.class);
     }
 }
