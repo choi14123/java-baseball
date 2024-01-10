460c9b173e7e5407a2cf91d951c6d752ce86725b
choi14123 <ckdghks5565@gmail.com>
2024-01-10 20:00:46 +0900

    test: "BaseballNumbersTest"

--- src/test/java/baseball/model/baseballnumber/BaseballNumbersTest.java
+++ src/test/java/baseball/model/baseballnumber/BaseballNumbersTest.java
@@ -3,7 +3,6 @@ package baseball.model.baseballnumber;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
@@ -26,7 +25,7 @@ class BaseballNumbersTest {
         assertThat(new BaseballNumbers(numbers).getNumbers().size()).isEqualTo(3);
     }
 
-    private static Stream<Arguments> validateSizeTest() {
+    private static Stream<Arguments> notThreeSizeNumbersProvide() {
         return Stream.of(
                 Arguments.of(Arrays.asList(1)),
                 Arguments.of(Arrays.asList(1, 2)),
@@ -34,42 +33,28 @@ class BaseballNumbersTest {
         );
     }
 
-    @ParameterizedTest
-    @MethodSource
-    @DisplayName("3자리 숫자가 아니면 IllegalArgumentException 예외 발생")
-    void validateSizeTest(List<Integer> number) {
-        //given
-        //when
-        List<Integer> numbers = new ArrayList<>(number);
+    private static Stream<Arguments> duplicationNumbersProvide() {
+        return Stream.of(Arguments.of(Arrays.asList(1, 1, 1)), Arguments.of(Arrays.asList(1, 2, 1)),
+                Arguments.of(Arrays.asList(1, 2, 2)));
+    }
 
-        //then
-        assertThatThrownBy(
-                () -> new BaseballNumbers(numbers))
+    @ParameterizedTest
+    @MethodSource("notThreeSizeNumbersProvide")
+    @DisplayName("객체를 생성할 때, 3자리 숫자가 아니면 IllegalArgumentException 예외 발생")
+    void validateSizeTest(List<Integer> notThreeSizeNumbersProvide) {
+        //when, then
+        assertThatThrownBy(() -> new BaseballNumbers(notThreeSizeNumbersProvide))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("3자리의 숫자를 입력해야합니다.");
     }
 
     @ParameterizedTest
-    @MethodSource
-    @DisplayName("중복 값이 있는지 확인 후 중복 값이 있으면 IllegalArgumentException 예외 발생")
-    void validateDuplicationValueTest(List<Integer> number) {
-        //given
-        //when
-        List<Integer> numbers = new ArrayList<>(number);
-
-        //then
-        assertThatThrownBy(
-                () -> new BaseballNumbers(numbers))
+    @MethodSource("duplicationNumbersProvide")
+    @DisplayName("객체를 생성 할 때, 중복 값이 있으면 IllegalArgumentException 예외 발생")
+    void validateDuplicationValueTest(List<Integer> duplicationNumbers) {
+        //when, then
+        assertThatThrownBy(() -> new BaseballNumbers(duplicationNumbers))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("중복된 숫자가 입력 되었습니다");
-    }
-
-    private static Stream<Arguments> validateDuplicationValueTest() {
-        return Stream.of(
-                Arguments.of(Arrays.asList(1, 1, 1)),
-                Arguments.of(Arrays.asList(1, 1, 2)),
-                Arguments.of(Arrays.asList(1, 2, 2)),
-                Arguments.of(Arrays.asList(2, 2, 2))
-        );
     }
 }
