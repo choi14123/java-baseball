1e98edc1761e8f2cdbb9609aa5df5ec2ace965d9
Choi Chang Hwan <63833865+choi14123@users.noreply.github.com>
2024-01-16 11:32:45 +0900

    style: "OutputView"

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,6 +1,5 @@
 package baseball.view;
 
-
 public class OutputView {
     private static final String PRINT_START_GAME_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String PRINT_INPUT_GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
@@ -30,7 +29,6 @@ public class OutputView {
     }
 
     public void printThreeStrike() {
-            System.out.println("3스트라이크" + "\n" +
-                    "3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 }
